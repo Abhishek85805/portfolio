5c18a26e0d23b81e3e96127b6d2c24b1fd1b0238
Abhishek85805 <abhishekchauhan85805@gmail.com>
2024-08-13 19:05:08 +0530

    commit

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -44,7 +44,7 @@ function Projects({ projectsRef }) {
   return (
     <div ref={projectsRef} className='min-h-full pt-[8rem] flex flex-col items-center p-[1.2rem]'>
       <div>
-        <h6 className='text-[#097FFF] font-extrabold mb-[1rem] md:px-[2rem]'>PORTFOLIO</h6>
+        <h6 className='text-[#097FFF] font-extrabold mb-[1rem] md:px-[2rem]'>PROJECTS</h6>
         <h2 className='font-extrabold text-[1.5rem] mb-[6rem] md:px-[2rem]'>Each project is a unique piece of development</h2>
         {
           data.map((project, index) => <ProjectComponent index={index} project={project} key={index} />)
